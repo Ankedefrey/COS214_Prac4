6dcc672f1f2a92c35b5e8e4c5cc75eddaeddbb4a
Terrence <u25446968@tuks.co.za>
2026-09-06 12:02:17 +0200

    Complete UnionCrewDecorator methods
    Implemented cost calculation and display method for UnionCrewDecorator.

--- UnionCrewDecorator.cpp
+++ UnionCrewDecorator.cpp
@@ -1,16 +1,18 @@
 #include "UnionCrewDecorator.h"
+#include <iostream>
+
+UnionCrewDecorator::UnionCrewDecorator(ProductionComponent* c) : ProductionDecorator(c) {}
 
 double UnionCrewDecorator::getCost() {
-	// TODO - implement UnionCrewDecorator::getCost
-	throw "Not yet implemented";
+	// Certified union crew adds a fixed surcharge on top of the wrapped cost.
+	const double surcharge = 500.0;
+	return component->getCost() + surcharge;
 }
 
 void UnionCrewDecorator::display(int depth) {
-	// TODO - implement UnionCrewDecorator::display
-	throw "Not yet implemented";
-}
-
-UnionCrewDecorator::UnionCrewDecorator(ProductionComponent* c) {
-	// TODO - implement UnionCrewDecorator::UnionCrewDecorator
-	throw "Not yet implemented";
+	for (int i = 0; i < depth; ++i) {
+		std::cout << "  ";
+	}
+	std::cout << "[Union Crew]" << std::endl;
+	component->display(depth + 1);
 }
