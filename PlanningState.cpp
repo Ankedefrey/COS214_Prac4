2ba3fe7f741b82998f402d19b9f93fa5622e6b58
Ankedefrey <ankedefrey@gmail.com>
2026-09-06 10:58:08 +0200

    set state to new shootingstate

--- PlanningState.cpp
+++ PlanningState.cpp
@@ -1,11 +1,11 @@
 #include "PlanningState.h"
+#include "ShootingState.h"
+#include "ProductionTask.h"
 
 void PlanningState::advance(ProductionTask* task) {
-	// TODO - implement PlanningState::advance
-	throw "Not yet implemented";
+	task->setState(new ShootingState);
 }
 
 string PlanningState::getName() {
-	// TODO - implement PlanningState::getName
-	throw "Not yet implemented";
+	return "Planning";
 }
